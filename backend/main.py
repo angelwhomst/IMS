38172772db6ac93c87c9be329276a7172bac02b0
angelwhomst <angelolivarez810@gmail.com>
2025-01-26 17:11:18 +0800

    changed port

--- backend/main.py
+++ backend/main.py
@@ -57,9 +57,5 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app", 
-        host="0.0.0.0",
-        port=8000, 
-        reload=True
-    )
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set  
+    uvicorn.run("main:app", host="0.0.0.0", port=port)  
